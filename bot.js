5c1d19fd9c59d0af207d6f6063574f710054d0e1
opsJson <cleversonpelegrini@gmail.com>
2023-04-07 03:15:15 -0300

    fix

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { EmbedBuilder, WebhookClient } = require("discord.js");
 const cheerio = require("cheerio");
-const fetch = require("node-fetch@2");
+const fetch = require("node-fetch");
 
 const posted = [];
 const url = "webhook-url";
